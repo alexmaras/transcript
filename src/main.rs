6f98477a1a586f7a2b62cc93f0b155dee01a11f5
georgiotech <gcroucamp@gmail.com>
2023-10-02 18:10:41 +0800

    feat: write transcription to .srt

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,14 @@ fn parse_wav_file(path: &Path) -> Vec<i16> {
         .collect::<Vec<_>>()
 }
 
+fn milliseconds_to_srt_time_string(time: i64) -> String {
+    let ms = time % 1000;
+    let seconds = (time / 1000) % 60;
+    let minutes = (time / 1000 / 60) % 60;
+    let hours = time / 1000 / 60 / 60;
+    format!("{:02}:{:02}:{:02},{:03}", hours, minutes, seconds, ms)
+}
+
 fn main() {
     let audio_file_path = Path::new("./samples/test.wav");
     if !audio_file_path.exists() {
@@ -58,6 +66,7 @@ fn main() {
 
     println!("{}", num_segments);
     
+    let mut srt_sequences: Vec<String> = Vec::new();
     let mut timestamped_lines: Vec<String> = Vec::new();
 
     for i in 0..num_segments {
@@ -65,11 +74,16 @@ fn main() {
         let start_timestamp = state.full_get_segment_t0(i).expect("failed to get segment start timestamp");
         let end_timestamp = state.full_get_segment_t1(i).expect("failed to get segment end timestamp");
 
+        let srt_start_timestamp = milliseconds_to_srt_time_string(start_timestamp);
+        let srt_end_timestamp = milliseconds_to_srt_time_string(end_timestamp);
+        let srt_formatted: String = format!("{}\n{} --> {}\n{}\n\n", i+1, srt_start_timestamp, srt_end_timestamp, segment);
+        srt_sequences.push(srt_formatted);
+
         let timestamped: String = format!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
         println!("{}", timestamped);
         timestamped_lines.push(format!("{}\n", timestamped));
     }
 
     write_to_file(Path::new("./transcribed.txt"), timestamped_lines);
-
+    write_to_file(Path::new("./transcribed.srt"), srt_sequences);
 }
