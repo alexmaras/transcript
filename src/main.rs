d7ec569d8a36586de92e67c62e3914eba55b6257
Alex Maras <dev@alexmaras.com>
2023-10-11 09:57:28 +0800

    feat: allow for passing wav on stdin

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use whisper_rs::{WhisperContext, FullParams, SamplingStrategy};
 use std::{path::Path, cmp};
 use std::fs::File;
+use std::io;
 use std::io::Write;
+use std::io::Read;
 use hound::{SampleFormat, WavReader};
 use std::env;
+use atty::Stream;
 
 fn write_to_file(path: &Path, lines: Vec<String>) {
     let mut file = File::create(path).expect("Could not create file");
@@ -12,9 +15,30 @@ fn write_to_file(path: &Path, lines: Vec<String>) {
     }
 }
 
-fn parse_wav_file(path: &Path) -> Vec<i16> {
-    let reader = WavReader::open(path).expect("failed to read file");
+fn construct_wavreader() -> WavReader<Box<dyn io::BufRead>> {
+    let args: Vec<String> = env::args().collect();
+    
+    if atty::is(Stream::Stdin) {
+        if args.len() < 2  {
+            panic!("No audio file passed");
+        }
+        let audio_file_path_raw = &args[1];
+        let audio_file_path = Path::new(audio_file_path_raw);
+        if !audio_file_path.exists() {
+            panic!("audio file doesn't exist");
+        }
+        let file = File::open(audio_file_path).expect("Failed to open audio file");
+        let buf_reader = io::BufReader::new(file);
+        return WavReader::new(Box::new(buf_reader) as Box<dyn io::BufRead>).expect("failed to read file");
+    } else {
+        let stdin = io::stdin();
+        let patched_reader = WavLengthPatcher::new(stdin);
+        let buf_reader = io::BufReader::new(patched_reader);
+        return WavReader::new(Box::new(buf_reader) as Box<dyn io::BufRead>).expect("Input is not valid wav");
+    };
+}
 
+fn parse_wav(reader: WavReader<Box<dyn io::BufRead>>) -> Vec<i16> {
     if reader.spec().channels != 1 {
         panic!("expected mono audio file");
     }
@@ -30,7 +54,20 @@ fn parse_wav_file(path: &Path) -> Vec<i16> {
 
     reader
         .into_samples::<i16>()
-        .map(|x| x.expect("sample"))
+        .map_while(|sample| match sample {
+            Ok(sample) => {
+                Some(sample)
+            },
+            Err(hound::Error::IoError(e)) => {
+                if let Some(ee) = e.get_ref() {
+                    if "Failed to read enough bytes." == &format!("{}", ee) {
+                        return None
+                    }
+                }
+                panic!("Error reading audio data: {:#?}", e);
+            }
+            Err(e) => panic!("Error reading audio data: {:#?}", e)
+        })
         .collect::<Vec<_>>()
 }
 
@@ -44,21 +81,16 @@ fn segment_time_to_srt_time_string(time: i64) -> String {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let audio_file_path_raw = &args[1];
+
     let model_path_raw = "./models/tiny.en.bin";
 
-    let audio_file_path = Path::new(audio_file_path_raw);
-    if !audio_file_path.exists() {
-        panic!("audio file doesn't exist");
-    }
     let model_path = Path::new(model_path_raw);
     if !model_path.exists() {
         panic!("model does not exist");
     }
 
-    let audio_data = parse_wav_file(audio_file_path);
+    let wavreader = construct_wavreader();
+    let audio_data = parse_wav(wavreader);
     let ingested_wav = whisper_rs::convert_integer_to_float_audio(&audio_data);
 
     println!("{}", &model_path.to_string_lossy());
@@ -106,5 +138,60 @@ mod tests {
         assert_eq!(segment_time_to_srt_time_string(838850), "02:19:48,500");
         assert_eq!(segment_time_to_srt_time_string(5602555), "15:33:45,550");
         assert_eq!(segment_time_to_srt_time_string(-4550), "00:00:00,000");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct WavLengthPatcher<R: Read> {
+    stream: R,
+    pos: u8,
+}
+
+impl <R: Read> WavLengthPatcher<R> {
+    fn new(stream: R) -> Self {
+        Self {
+            stream,
+            pos: 0,
+        }
+    }
+}
+
+impl <R: Read> Read for WavLengthPatcher<R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let l = self.stream.read(buf)?;
+        if self.pos < 0x4e {
+            // fake bytes @ 0x04 and 0x4A from 0xFF -> 0xF0 (zero lowest 4 bits of file & data chunk length)
+            let capped_l = l.min(0x4e) as u8;
+            for p in 0 .. capped_l {
+                match p + self.pos {
+                    0x04 | 0x4A => {
+                        assert_eq!(buf[p as usize], 0xFF);
+                        buf[p as usize] = 0xF0;
+                    }
+                    0x05 | 0x06 | 0x07 | 0x4b | 0x4c | 0x4d => {
+                        assert_eq!(buf[p as usize], 0xFF);
+                    }
+                    _ => ()
+                }
+            }
+            self.pos += capped_l; // cannot overflow
+        }
+        Ok(l)
     }
 }
